47a904516ece3aa1eaeca29ab21e55facf3862c1
郇永超 <5422156+yong_chao_ling@user.noreply.gitee.com>
2021-09-15 23:12:56 +0800

    1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '59130fae120d2c08bcd8169c3ad81f86bcd05081',
+        'name' => 'monkeyh/pdo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monkeyh/pdo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '59130fae120d2c08bcd8169c3ad81f86bcd05081',
+            'dev_requirement' => false,
+        ),
+    ),
+);
